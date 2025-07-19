cb83049d95d96071ae9386410f7cbc7eead4549c
Arseniy-1 <92578166+Arseniy-1@users.noreply.github.com>
2025-07-19 09:06:25 +0300

    1

--- Assets/Project/Scripts/UI/View/UserInfoView.cs
+++ Assets/Project/Scripts/UI/View/UserInfoView.cs
@@ -1,4 +1,5 @@
 ﻿using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using YG;
 
@@ -27,8 +28,27 @@ public class UserInfoView : MonoBehaviour
         _rankAmountBar.Initialize(_rankHolder);
         _rankAmountTextView.Initialize(_rankHolder);
 
-        // if (YG2.infoYG.Authorization.authorized)
-        _userName.text = YG2.player.name;
+        if (YG2.infoYG.Authorization.authorized)
+        {
+            _userName.text = YG2.player.name;
+        }
+        else
+        {
+            switch (YG2.lang)
+            {
+                case nameof(Lanquages.en):
+                    _userName.text = "unauthorized";   
+                    break;
+                
+                case nameof(Lanquages.ru):
+                    _userName.text = "не авторизован";   
+                    break;
+                
+                case nameof(Lanquages.tr):
+                    _userName.text = "Yetkili değil";   
+                    break;
+            }
+        }
     }
 
     private void HandleRankRaised()
